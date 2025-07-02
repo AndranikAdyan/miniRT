71a8a3fa97c5bf95da010f4d30177d2581a3268d
Andranik Adyan <andranikadyan@gmail.com>
2025-07-02 20:57:48 +0400

    add structs

--- initilization/initilization.h
+++ initilization/initilization.h
@@ -15,9 +15,47 @@
 
 # include "miniRT.h"
 
-typedef struct s_mlx	t_mlx;
+typedef struct  s_rgb
+{
+        unsigned char   red;
+        unsigned char   green;
+        unsigned char   blue;
+}   t_rgb;
 
-t_mlx	*init_mlx(void);
-int		free_mlx(t_mlx *mlx);
+typedef enum    e_obj_type
+{
+        SPHERE,
+        PLANE,
+        CYLINDER,
+}       t_obj_type;
+
+typedef struct  s_vec
+{
+        double  x;
+        double  y;
+        double  z;
+}       t_vec;
+
+typedef struct  s_camera
+{
+        t_vec   pos;
+        t_vec   dir;
+        int     fov;
+}       t_camera;
+
+typedef struct  s_object
+{
+        t_obj_type  obj_type;
+        t_vec   position;
+        t_vec   dir;
+        double  radius;
+        double  height;
+        t_rgb   color;
+}       t_object;
+
+typedef struct s_mlx    t_mlx;
+
+t_mlx   *init_mlx(void);
+int     free_mlx(t_mlx *mlx);
 
 #endif
