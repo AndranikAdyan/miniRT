022156322ba5d6d26bc403381b4af05163239131
AndranikAdyan <andranikadyan@gmail.com>
2025-07-03 13:29:11 +0400

    make vector formulas with pointers

--- formulas/vectors/vectors_formulas.h
+++ formulas/vectors/vectors_formulas.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 01:15:33 by aadyan            #+#    #+#             */
-/*   Updated: 2025/07/03 01:26:55 by aadyan           ###   ########.fr       */
+/*   Updated: 2025/07/03 13:30:18 by aadyan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 # include "scene.h"
 
-double	dot_product(t_vec a, t_vec b);
-t_vec	scalar_product(t_vec a, double num);
+double	dot_product(t_vec *a, t_vec *b);
+t_vec	scalar_product(t_vec *a, double num);
+t_vec	cross_product(t_vec *a, t_vec *b);
+double	vec_length(t_vec *a);
+t_vec	normalize(t_vec *a);
 
 #endif
