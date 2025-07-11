cc04f2a6643c2b4956f45a355d815a54098422c1
assam04 <samaslanyan3@gmail.com>
2025-07-11 12:59:41 +0400

    changes about calculating color

--- render/drawing.c
+++ render/drawing.c
@@ -6,31 +6,34 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 00:47:33 by aadyan            #+#    #+#             */
-/*   Updated: 2025/07/11 12:00:40 by saslanya         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:58:51 by saslanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "intersection.h"
 
-static t_rgb	calculate_color(t_scene *scene, double x, double y)
+static int	get_color(t_scene *scene, double x, double y)
 {
-	double	min;
-	t_rgb	color;
-	t_list	*obj_list;
-	t_rgb	tmp_color;
+	t_list	*iter;
+	t_hit	hit;
 
-	color = (t_rgb){0, 0, 0};
-	min = INFINITY;
-	obj_list = scene->objects;
-	while (obj_list)
+	iter = scene->objects;
+	ft_bzero(&hit, sizeof(t_hit));
+	while (iter)
 	{
-		tmp_color = intersection(scene, obj_list, &min, (double []){x, y});
-		if (!(tmp_color.red == 0 && tmp_color.green == 0
-				&& tmp_color.blue == 0))
-			color = tmp_color;
-		obj_list = obj_list->next;
+		intersection_with_object(scene, iter, &hit, (double [])(x, y));
+		iter = iter->next;
 	}
-	return (color);
+	if (!hit.intersection)
+		return (BACKGORUND_COLOR);
+	iter = scene->lights;
+	while (iter)
+	{
+		if (((t_light *)iter->content)->type != AMBIENT)
+			intersection_with_light(scene, iter, &hit);
+		iter = iter->next;
+	}
+	return (hit.color);
 }
 
 void	draw_frame(t_mlx *mlx)
@@ -44,12 +47,9 @@ void	draw_frame(t_mlx *mlx)
 	{
 		x = -1.0;
 		while (++x < WIN_WEIGHT)
-		{
-			color = calculate_color(mlx->scene, x, y);
 			*(int *)(mlx->img_data->addr + ((int)y * mlx->img_data->line_length
 						+ (int)x * (mlx->img_data->bits_per_pixel / 8)))
-				= (color.red << 16) | (color.green << 8) | color.blue;
-		}
+				= get_color(mlx->scene, x, y);
 	}
 	mlx_put_image_to_window(mlx->mlx, mlx->window, mlx->img_data->img, 0, 0);
 }
--- render/intersection.h
+++ render/intersection.h
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 00:09:07 by aadyan            #+#    #+#             */
-/*   Updated: 2025/07/11 11:59:48 by saslanya         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:58:31 by saslanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,23 @@
 # include "ray.h"
 # include "miniRT.h"
 
-t_rgb	intersection(t_scene *scene, t_list *obj_list,
-			double *min, double *xy);
+# define BACKGORUND_COLOR 0x000000
+
+typedef struct s_hit
+{
+	bool		intersection;  // hatum ka te che
+	int			color; // pixeli veradarcvox guyn
+	double		distance; // jaragaytic minchev hatman ket heravorytun
+	t_object	*figure;  // en  firgur@ vori het hatvel enq (normal u mnacac hashvarkneri hamar)
+	t_vec		point;    // hatman kkordinatner
+	t_vec		normal;   // ketic u obyketic kaxvac normal
+
+
+}	t_hit;
+
+void	intersection_with_object(t_scene *scene, t_object *figure,
+			t_hit *hit,  double *xy);
+void	intersection_with_light(t_scene *scene, t_light *light, t_hit *hit);
 void	draw_frame(t_mlx *mlx);
 
 #endif
