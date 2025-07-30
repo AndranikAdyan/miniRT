f58695c19b977f43649daab63a650607b23d6216
assam04 <samaslanyan3@gmail.com>
2025-07-30 19:23:31 +0400

    correcting cylinder board

--- render/src/cy_calcs.c
+++ render/src/cy_calcs.c
@@ -6,7 +6,7 @@
 /*   By: aadyan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 00:59:33 by saslanya          #+#    #+#             */
-/*   Updated: 2025/07/26 22:28:10 by aadyan           ###   ########.fr       */
+/*   Updated: 2025/07/30 19:22:46 by saslanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,16 +90,28 @@ double	intersect_body(t_cylinder *cy, t_vec ray, t_vec cam)
 
 void	get_cy_color(t_cylinder *cy, t_hit *hit)
 {
-	t_vec	rel;
-	double	u;
-	double	v;
-	double	coeff;
+	t_vec	v[2];
+	double	val[3];
 
-	rel = vec_sub(hit->point, cy->pos);
-	u = atan2(rel.z, rel.x) / (2 * M_PI) + 0.5;
-	v = dot_product(rel, cy->dir) / cy->height;
-	coeff = (double)(((int)(u * 10) + (int)(v * 10)) % 2 == 0);
-	hit->color.red = 1.0 * coeff;
-	hit->color.green = 1.0 * coeff;
-	hit->color.blue = 1.0 * coeff;
+	v[0] = vec_sub(hit->point, vec_add(cy->pos,
+				scalar_product(cy->dir, cy->height / 2.0)));
+	if (fabs(cy->dir.x) > fabs(cy->dir.z))
+		v[1] = normalize((t_vec){-cy->dir.y, cy->dir.x, 0});
+	else
+		v[1] = normalize((t_vec){0, -cy->dir.z, cy->dir.y});
+	val[0] = atan2(dot_product(v[0], normalize(cross_product(cy->dir,
+						v[1]))), dot_product(v[0], v[1])) / (2 * M_PI);
+	if (val[0] < 0)
+		val[0] += 1.0;
+	val[1] = dot_product(v[0], cy->dir) / cy->height;
+	val[0] = fmod(val[0], 1.0);
+	if (val[0] < 0)
+		val[0] += 1.0;
+	val[1] = fmod(val[1], 1.0);
+	if (val[1] < 0)
+		val[1] += 1.0;
+	val[2] = (double)((int)(floor(val[0] * 10) + (int)floor(val[1] * 10)) % 2);
+	hit->color.red = val[2];
+	hit->color.green = val[2];
+	hit->color.blue = val[2];
 }
